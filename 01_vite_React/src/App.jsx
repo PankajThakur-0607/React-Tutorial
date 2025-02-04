cfa9b3b4afca0663ec25487544b708f8e552346d
Pankaj Thakur <pankajothakur456@gmail.com>
2025-02-04 15:01:49 +0530

    create a custom React Library

--- 01_vite_React/src/App.jsx
+++ 01_vite_React/src/App.jsx
@@ -1,9 +1,11 @@
 import Chai from "./chai";
 
 function App() {
+  const username = "Pankaj Thakur";
   return (
     <>
       <Chai />
+      I am {username}
       <h1>React Tutorial with Vite | Pankaj Thakur</h1>
     </>
   );
--- 01_vite_React/src/main.jsx
+++ 01_vite_React/src/main.jsx
@@ -1,12 +1,40 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <>
-      <h1>Lets Get Started</h1>
-      <App />
-    </>
-  </StrictMode>
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App </h1>
+    </div>
+  );
+}
+
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://www.google.com",
+//     target: "_blank",
+//   },
+//   children: "click me to Visit Google",
+// };
+
+const anotherElement = (
+  <a href="https://www.google.com" target="_blank">Visit to Google! </a>
+)
+  
+
+const anotherUser = "Monisa Kingh"
+const reactElement = React.createElement(
+  'a',
+  {href : 'https://google.com' , target : '_blank'},
+  'click to visit Google !',
+  anotherUser
+)
+
+
+createRoot(document.getElementById("root"))
+.render(
+    reactElement
 );
